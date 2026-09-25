06c2ce014e784a4330dfc7b0588cacb56cffe19a
oliver-strandvall <oliver.strandvall@optistud.fi>
2026-09-25 11:01:13 +0300

    25/9/2026: 1.6-1.11 Done

--- part1/1.6-1.11/src/App.jsx
+++ part1/1.6-1.11/src/App.jsx
@@ -1,38 +1,34 @@
 import { useState } from "react"
-// import Statistics from "./Components/Statistics"
+import Statistics from "./Components/Statistics"
+import Button from "./Components/Button"
 
 const App = () => {
-  let [good, setGood] = useState(0);
-  let [neutral, setNeutral] = useState(0);
-  let [bad, setBad] = useState(0);
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   function setGoodFeedback() {
-    setGood((good) => good + 1);
+      setGood((good) => good + 1);
   }
 
   function setNeutralFeedback() {
-    setNeutral((neutral) => neutral + 1);
+      setNeutral((neutral) => neutral + 1);
   }
 
   function setBadFeedback() {
-    setBad((bad) => bad + 1);
+      setBad((bad) => bad + 1);
   }
 
   return (
     <div>
       <h1>Give Feedback</h1>
       <div>
-        <button onClick={setGoodFeedback}>Good</button>
-        <button onClick={setNeutralFeedback}>Neutral</button>
-        <button onClick={setBadFeedback}>Bad</button>
+        <Button text="Good" onClick={setGoodFeedback}></Button>
+        <Button text="Neutral" onClick={setNeutralFeedback}></Button>
+        <Button text="Bad" onClick={setBadFeedback}></Button>
       </div>
       <h1>Statistics</h1>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>Total: {good + neutral + bad}</p>
-      <p>Average: {good + neutral + bad === 0 ? 0 : (good - bad) / (good + neutral + bad)}</p>
-      <p>Positive: {good + neutral + bad === 0 ? 0 : (good / (good + neutral + bad)) * 100}%</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
--- part1/1.6-1.11/src/Components/Statistics.jsx
+++ part1/1.6-1.11/src/Components/Statistics.jsx
@@ -1,3 +1,29 @@
-// export default function Statistics() {
+import StatisticLine from "./StatisticLine"
 
-// }
+const Statistics = ({ good, neutral, bad }) => {
+    const total = good + neutral + bad
+
+    if(total === 0) {
+        return(
+            <p>No Feedback Given</p>
+        )
+    }
+
+    const average = (good - bad) / total
+    const positive = (good / total) * 100
+  
+    return(
+        <table>
+            <tbody>
+                <StatisticLine text="good" value={good} />
+                <StatisticLine text="neutral" value={neutral} />
+                <StatisticLine text="bad" value={bad} />
+                <StatisticLine text="total" value={total} />
+                <StatisticLine text="average" value={average} />
+                <StatisticLine text="positive" value={`${positive}%`} />
+            </tbody>
+        </table>
+    )
+}
+
+export default Statistics
